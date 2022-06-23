a76b78c38fa97144ddf2d71d8722c7a585821984
Dillon Brock <mdillon1297@gmail.com>
2022-06-23 11:56:04 -0700

    added filters and modifiers to getDogs function

--- services/adopt-service.js
+++ services/adopt-service.js
@@ -6,11 +6,18 @@ export async function getDogs(breed, age, { start, end }) {
     let query = client
         .from('dogs')
         // *** set option to return an exact count
-        .select('id, name, breed');
+        .select('id, name, breed, age', { count: 'exact' });
 
     // *** add breed and age filters if they exist
+    if (breed) {
+        query = query.ilike('breed', `%${breed}%`);
+    }
 
+    if (age) {
+        query = query.ilike('age', `%${age}%`);
+    }
     // *** add paging by setting a range modifier
+    query = query.range(start, end);    
 
     const response = await query;
 
